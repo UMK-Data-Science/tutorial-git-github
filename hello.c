545429c5a54e756d256d17fd08627f8259a0f006
aqillakhamis <aqillakhamis@gmail.com>
2022-03-27 23:57:32 +0800

    update file

---
+++ hello.c
@@ -0,0 +1,15 @@
+//documentation section
+/* this is your first tutorial using Git and GitHub */
+
+//pre-processor section
+#include<stdio.h>
+
+//global variable section 
+
+//main function section 
+int main(){
+    //write your program here
+    
+}
+
+//user defined section 
